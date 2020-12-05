f0a6a531cfeb32e88276e1dfda40011c9167099c
Jeroen Vloothuis <jeroen@9bitcat.com>
2020-12-05 11:06:39 +0100

    Use ASDF instead of Docker for development
    This greatly improves the development experience (speed for tests
    & dev server).

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 # Configure your database
 config :link, Link.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "link",
+  password: "supersecret",
   database: "link_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
