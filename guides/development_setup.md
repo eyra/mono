8b4a5dd81d43479e89a379338a312b245387066f
Jeroen Vloothuis <jeroen@9bitcat.com>
2020-12-06 14:18:14 +0100

    Tweaks to setup script for ASDF

--- guides/development_setup.md
+++ guides/development_setup.md
@@ -4,6 +4,8 @@
 
 For Windows; first install [WSL2](https://docs.microsoft.com/en-us/windows/wsl/install-win10) and the [Windows Terminal](https://docs.microsoft.com/nl-nl/windows/terminal/). Run all steps in the WSl2 environment.
 
+For OS X; install GnuPG, `brew install gnupg`.
+
 1. Install Visual Studio Code (or another editor)
 2. Checkout out the code locally
 3. [Install ASDF](https://asdf-vm.com/#/core-manage-asdf?id=install)
--- setup.sh
+++ setup.sh
@@ -1,22 +1,29 @@
 #!/bin/sh -e
 
-asdf plugin-add erlang
-asdf plugin-add elixir
-asdf plugin-add postgres
-asdf plugin-add nodejs
-asdf plugin-add python
+# Temp hack for https://bugs.erlang.org/browse/ERL-1407
+export MACOSX_DEPLOYMENT_TARGET=10.0
+
+bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
+bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-previous-release-team-keyring'
+
+asdf plugin-add erlang || true
+asdf plugin-add elixir || true
+asdf plugin-add postgres || true
+asdf plugin-add nodejs || true
 asdf install
 
 cd assets
-npm install
+asdf exec npm install
 cd ..
 
-mix deps.get
+asdf exec mix deps.get
 
 if [ ! -d db ]
 then
-    initdb --username=link db
-    pg_ctl -D db start
-    createdb --owner=link --username=link link_dev 
-    pg_ctl -D db stop
+    asdf exec initdb --username=link db
+    asdf exec pg_ctl -D db start
+    asdf exec createdb --owner=link --username=link link_dev
+    asdf exec pg_ctl -D db stop
 fi
+
+echo "All done. Reload your shell to enable the new commands."
