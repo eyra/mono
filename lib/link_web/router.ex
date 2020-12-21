45d16dc1611d8e9eeb9ab2836ef888ddbd7115ae
Jeroen Vloothuis <jeroen@9bitcat.com>
2020-12-21 10:24:45 +0000

    Use text fields for study title and description

--- lib/link_web/router.ex
+++ lib/link_web/router.ex
@@ -67,7 +67,6 @@ defmodule LinkWeb.Router do
 
   scope "/", LinkWeb do
     pipe_through :browser
-
   end
 
   scope "/", LinkWeb do
@@ -105,7 +104,7 @@ defmodule LinkWeb.Router do
 
     scope "/" do
       pipe_through :browser
-      live_dashboard "/dashboard", metrics: LinkWeb.Telemetry
+      live_dashboard "/phoenix-dashboard", metrics: LinkWeb.Telemetry
     end
   end
 end
