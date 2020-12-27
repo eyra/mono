f42ff5d276363dbfc28667e1ea9e71cb4c6b6e5a
Jeroen Vloothuis <jeroen@9bitcat.com>
2020-12-27 17:54:37 +0100

    MInor script tweaks

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 #!/bin/sh -e
 
-bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
-bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-previous-release-team-keyring'
-
 asdf plugin-add erlang || true
 asdf plugin-add elixir || true
 asdf plugin-add nodejs || true
+
+bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
+bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-previous-release-team-keyring'
+
 asdf install
 
 cd assets
