0384beab3b2ef95486fe3ec6af1411a15b47c041
Jeroen Vloothuis <jeroen@9bitcat.com>
2021-01-10 14:17:16 +0100

    Added card component

--- lib/eyra_ui/usp_card.ex
+++ lib/eyra_ui/usp_card.ex
@@ -3,25 +3,25 @@ defmodule EyraUI.USPCard do
   The Unique Selling Point Card highlights a reason for taking an action.
   """
   use Surface.Component
+  alias EyraUI.Card
 
   prop title, :string, required: true
   prop description, :string, required: true
   prop title_color, :css_class, default: "text-grey1"
   prop description_color, :css_class, default: "text-grey2"
-  prop bg_color, :css_class, default: "bg-grey6"
 
   def render(assigns) do
     ~H"""
-    <div class={{ @bg_color, "h-full", "rounded-md" }}>
-      <div class="p-6 lg:pl-8 lg:pr-8 lg:pt-10 lg:pb-10">
+    <Card size="h-full">
+      <template slot="title">
         <div class={{"text-title5", "font-title5", "lg:text-title4", "lg:font-title4", "mb-4", "lg:mb-6", @title_color }}>
-            {{ @title }}
+              {{ @title }}
         </div>
-        <div class={{ "text-bodysmall", "lg:text-bodymedium", "font-body", @description_color }}>
-            {{ @description }}
-        </div>
+      </template>
+      <div class={{ "text-bodysmall", "lg:text-bodymedium", "font-body", @description_color }}>
+          {{ @description }}
       </div>
-    </div>
+    </Card>
     """
   end
 end
