22073b3bdccecda96bc8bd5a768f16fe4cc82bd4
emielvdveen <emiel@applike.nl>
2021-03-11 13:15:38 +0100

    Moved all tests exept CoreWeb from Link to Core

--- apps/core/mix.exs
+++ apps/core/mix.exs
@@ -36,7 +36,7 @@ defmodule Core.MixProject do
   end
 
   # Specifies which paths to compile per environment.
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(:test), do: ["lib", "test", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
