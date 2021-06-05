1161052440f7a34e5413a15b83bafadf6182ee86
Jeroen Vloothuis <jeroen@9bitcat.com>
2021-06-05 10:45:25 +0200

    Changed links to live redirects to improve speed

--- frameworks/eyra_ui/lib/components/button/primary_icon_button.ex
+++ frameworks/eyra_ui/lib/components/button/primary_icon_button.ex
@@ -3,15 +3,16 @@ defmodule EyraUI.Button.PrimaryIconButton do
   A colored button with white text
   """
   use Surface.Component
+  alias Surface.Components.LiveRedirect
 
-  prop(path, :string, required: true)
+  prop(to, :string, required: true)
   prop(label, :string, required: true)
   prop(icon, :string, required: true)
   prop(bg_color, :string, required: true)
 
   def render(assigns) do
     ~H"""
-    <a href= {{ @path }} >
+    <LiveRedirect to={{ @to }} >
       <div class="pt-1 pb-1 active:pt-5px active:pb-3px active:shadow-top4px w-full rounded pl-4 pr-4 {{@bg_color}}">
         <div class="flex justify-center items-center w-full">
           <div>
@@ -26,7 +27,7 @@ defmodule EyraUI.Button.PrimaryIconButton do
           </div>
         </div>
       </div>
-    </a>
+    </LiveRedirect>
     """
   end
 end
--- frameworks/eyra_ui/lib/components/button/primary_wide_button.ex
+++ frameworks/eyra_ui/lib/components/button/primary_wide_button.ex
@@ -3,14 +3,15 @@ defmodule EyraUI.Button.PrimaryWideButton do
   A colored button with white text
   """
   use Surface.Component
+  alias Surface.Components.LiveRedirect
 
-  prop(path, :string, required: true)
+  prop(to, :string, required: true)
   prop(label, :string, required: true)
   prop(bg_color, :string, required: true)
 
   def render(assigns) do
     ~H"""
-    <a href= {{ @path }} >
+    <LiveRedirect to={{ @to }} >
       <div class="flex w-full {{@bg_color}} rounded justify-center items-center pl-4 pr-4">
         <div class="pt-15px pb-15px active:pt-4 active:pb-14px active:shadow-top4px focus:outline-none">
           <div class="flex flex-col justify-center h-full items-center rounded">
@@ -20,7 +21,7 @@ defmodule EyraUI.Button.PrimaryWideButton do
           </div>
         </div>
       </div>
-    </a>
+    </LiveRedirect>
     """
   end
 end
