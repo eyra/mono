3f321b7f38046e26b7d76d3ef15cd25e8a31f789
Jeroen Vloothuis <jeroen@9bitcat.com>
2021-08-14 14:26:03 +0200

    Ensure feature flags are only used at runtime (#116)
    This fixes issues with gettext extraction due to compilation differences.

--- core/lib/core_web/router.ex
+++ core/lib/core_web/router.ex
@@ -1,6 +1,5 @@
 defmodule CoreWeb.Router do
   use CoreWeb, :router
-  import Core.FeatureFlags
 
   require Core.BundleOverrides
   require Core.SurfConext
@@ -9,15 +8,11 @@ defmodule CoreWeb.Router do
 
   Core.BundleOverrides.routes()
 
-  if feature_enabled?(:google_sign_in) do
-    require GoogleSignIn
-    GoogleSignIn.routes(:core)
-  end
+  require GoogleSignIn
+  GoogleSignIn.routes(:core)
 
-  if feature_enabled?(:sign_in_with_apple) do
-    require SignInWithApple
-    SignInWithApple.routes(:core)
-  end
+  require SignInWithApple
+  SignInWithApple.routes(:core)
 
   Core.SurfConext.routes(:core)
 
--- core/test/core/feature_flags_test.exs
+++ core/test/core/feature_flags_test.exs
@@ -1,10 +1,10 @@
 defmodule Core.FeatureFlagsTest do
   use Core.DataCase, async: true
-  alias Core.FeatureFlags
+  use Core.FeatureFlags
 
   describe "feature_enabled?/1" do
     test "return true by default" do
-      assert FeatureFlags.feature_enabled?(:some_unconfigured_flag)
+      assert feature_enabled?(:some_unconfigured_flag)
     end
 
     test "allow configuration via the application env" do
@@ -16,7 +16,26 @@ defmodule Core.FeatureFlagsTest do
         Keyword.put(current_env, :a_feature_used_by_this_test, false)
       )
 
-      refute FeatureFlags.feature_enabled?(:a_feature_used_by_this_test)
+      refute feature_enabled?(:a_feature_used_by_this_test)
+    end
+  end
+
+  describe "require_feature/1" do
+    test "do nothing when feature is enabled" do
+      require_feature(:some_unconfigured_flag)
+    end
+
+    test "throw when feature is disabled" do
+      current_env = Application.get_env(:core, :features, [])
+
+      Application.put_env(
+        :core,
+        :features,
+        Keyword.put(current_env, :a_feature_used_by_this_test, false)
+      )
+
+      assert catch_throw(require_feature(:a_feature_used_by_this_test)) ==
+               "Feature: a_feature_used_by_this_test is required"
     end
   end
 end
