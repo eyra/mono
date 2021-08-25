7bf25c45d0a25fc22f9b41a8201a465fc85d87d5
Jeroen Vloothuis <jeroen@9bitcat.com>
2021-08-25 21:26:15 +0200

    Removed unneeded wrapper funcs

--- core/assets/js/100vh-fix.js
+++ core/assets/js/100vh-fix.js
@@ -2,12 +2,8 @@ function setDocHeight() {
     document.documentElement.style.setProperty('--vh', `${window.innerHeight/100}px`);
   };
 
-  window.addEventListener('resize', function () {
-    setDocHeight();
-   });
+  window.addEventListener('resize', setDocHeight);
 
-  window.addEventListener('orientationchange', function () {
-    setDocHeight();
-  });
+  window.addEventListener('orientationchange', setDocHeight);
 
   setDocHeight();
