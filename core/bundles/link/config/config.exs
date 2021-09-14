15ebb06b63023f2fe3f580b45865f10d996a65d0
Jeroen Vloothuis <jeroen@9bitcat.com>
2021-09-14 09:23:20 +0200

    Disable notification mails for now

--- core/bundles/link/config/config.exs
+++ core/bundles/link/config/config.exs
@@ -18,7 +18,8 @@ config :core, CoreWeb.UserAuth,
 config :core, :features,
   sign_in_with_apple: false,
   google_sign_in: false,
-  password_sign_in: false
+  password_sign_in: false,
+  notification_mails: false
 
 config :core, Core.SurfConext, limit_schac_home_organization: "replace-with-vu"
 
