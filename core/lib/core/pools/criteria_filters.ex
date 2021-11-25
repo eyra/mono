59f34a33c230c29b850a03d0a68cd1cf1904b8fb
emielvdveen <emiel@applike.nl>
2021-11-25 17:44:27 +0100

    Student filter

---
+++ core/lib/core/pools/criteria_filters.ex
@@ -0,0 +1,22 @@
+defmodule Core.Pools.CriteriaFilters do
+  @moduledoc """
+  Defines filters used to filter students in the overview page of the pool.
+  """
+  use Core.Enums.Base,
+      {:criteria_filters, [:iba, :bk, :year1, :year2, :resit]}
+
+  def include?(codes, filters) when is_list(codes) and is_list(filters) do
+    Enum.count(Enum.filter(filters, &include?(codes, &1))) > 0
+  end
+
+  def include?(codes, filter) when is_list(codes) do
+    Enum.count(Enum.filter(codes, &include?(&1, filter))) > 0
+  end
+
+  def include?(code, :year1), do: String.contains?(Atom.to_string(code), "_1")
+  def include?(code, :year2), do: String.contains?(Atom.to_string(code), "_2")
+  def include?(code, :iba), do: String.contains?(Atom.to_string(code), "iba")
+  def include?(code, :bk), do: String.contains?(Atom.to_string(code), "bk")
+  def include?(code, :resit), do: String.contains?(Atom.to_string(code), "_h")
+  def include?(_, _), do: false
+end
