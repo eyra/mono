6ace043dd2c01f28ca979d4e1ce50bb43d8a9faa
emielvdveen <emiel@applike.nl>
2021-12-03 22:56:53 +0100

    Small UI fix

--- core/lib/core_web/ui/user_list_item_small.ex
+++ core/lib/core_web/ui/user_list_item_small.ex
@@ -15,7 +15,7 @@ defmodule CoreWeb.UI.UserListItemSmall do
         <div class="flex-shrink-0">
           <img src={{ImageHelpers.get_photo_url(@user.profile)}} class="rounded-full w-8 h-8 border-2 border-grey4 border-grey4" alt="" />
         </div>
-        <div class="flex-grow font-label text-label text-grey1 mt-1px">
+        <div class="flex-grow font-label text-label text-grey1 mt-2px">
           {{@user.profile.fullname}}
         </div>
         <div class="flex-wrap flex-shrink-0">
