96fbb5585ce4916db3039f472fab427863a98f1c
emielvdveen <emiel@applike.nl>
2022-02-16 23:29:12 +0100

    Disabled inactive users query

--- core/systems/pool/views/dashboard_view.ex
+++ core/systems/pool/views/dashboard_view.ex
@@ -15,15 +15,15 @@ defmodule Systems.Pool.DashboardView do
     pool = Core.Pools.get_by_name(:vu_students)
     first_year_criteria = %{study_program_codes: [:iba_1, :bk_1]}
     first_year_rewards = Campaign.Context.list_user_rewards(pool, first_year_criteria)
-    first_year_inactive_students = Campaign.Context.list_inactive_users(first_year_criteria)
+    # first_year_inactive_students = Campaign.Context.list_inactive_users(first_year_criteria)
 
     second_year_criteria = %{study_program_codes: [:iba_2, :bk_2]}
     second_year_rewards = Campaign.Context.list_user_rewards(pool, second_year_criteria)
-    second_year_inactive_students = Campaign.Context.list_inactive_users(second_year_criteria)
+    # second_year_inactive_students = Campaign.Context.list_inactive_users(second_year_criteria)
 
     years = [
-      create_year(:first, first_year_inactive_students, first_year_rewards),
-      create_year(:second, second_year_inactive_students, second_year_rewards)
+      create_year(:first, first_year_rewards),
+      create_year(:second, second_year_rewards)
     ]
 
     {
@@ -32,17 +32,17 @@ defmodule Systems.Pool.DashboardView do
     }
   end
 
-  defp create_year(year, inactive_students, credits) do
+  defp create_year(year, credits) do
     year_string = Core.Enums.StudyProgramCodes.year_to_string(year)
 
     %{
       title: dgettext("link-studentpool", "year.label", year: year_string),
       rows: [
-        %{
-          icon: "💤",
-          title: dgettext("link-studentpool", "inactive.students"),
-          value: Enum.count(inactive_students)
-        },
+        # %{
+        #   icon: "💤",
+        #   title: dgettext("link-studentpool", "inactive.students"),
+        #   value: Enum.count(inactive_students)
+        # },
         %{
           icon: "↓",
           title: dgettext("link-studentpool", "min.credits.earned.label"),
