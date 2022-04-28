1d340cbb80d0ccfffbe863d5a725a20310871839
emielvdveen <emiel@applike.nl>
2022-04-28 23:57:26 +0200

    Small improvement in import rewards feature

--- core/mix.exs
+++ core/mix.exs
@@ -44,7 +44,7 @@ defmodule Core.MixProject do
   def application do
     [
       mod: {Core.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :csv]
     ]
   end
 
