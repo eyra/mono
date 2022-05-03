c0a3038d3276c43ec5ee218be7b81543f6bb90b3
emielvdveen <emiel@applike.nl>
2022-05-03 18:24:00 +0200

    Possible fix for inactive towel feature

--- core/systems/campaign/expiration_worker.ex
+++ core/systems/campaign/expiration_worker.ex
@@ -1,8 +1,11 @@
 defmodule Systems.Campaign.ExpirationWorker do
   use Oban.Worker
 
+  require Logger
+
   @impl Oban.Worker
   def perform(%Oban.Job{args: _args}) do
+    Logger.warn("Running Towel Feature")
     Systems.Crew.Context.mark_expired()
   end
 end
