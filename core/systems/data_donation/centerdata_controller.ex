2901b786ea749cc8b7846040d72c859039f6e617
emielvdveen <emiel@applike.nl>
2022-06-10 13:29:51 +0200

    Added support for locale in post param from centerdata

--- core/systems/data_donation/centerdata_controller.ex
+++ core/systems/data_donation/centerdata_controller.ex
@@ -1,22 +1,30 @@
 defmodule Systems.DataDonation.CenterdataController do
   use CoreWeb, :controller
 
-  def create(
-        conn,
-        %{
-          "error1" => error1,
-          "id" => id,
-          "lang" => lang,
-          "mobile" => mobile,
-          "page" => page,
-          "questiontext1" => questiontext1,
-          "respondent" => respondent,
-          "token" => token,
-          "url" => url,
-          "varname1" => varname1,
-          "varvalue1" => varvalue1
-        }
-      ) do
+  @supported_locales ~w(nl en)
+
+  def create(conn, %{"id" => id} = params) do
+    []
+    |> add_locale(params)
+    |> add_session(params)
+    |> start_data_donation(conn, id)
+  end
+
+  defp add_session(
+         opts,
+         %{
+           "error1" => error1,
+           "lang" => lang,
+           "mobile" => mobile,
+           "page" => page,
+           "questiontext1" => questiontext1,
+           "respondent" => respondent,
+           "token" => token,
+           "url" => url,
+           "varname1" => varname1,
+           "varvalue1" => varvalue1
+         }
+       ) do
     session = %{
       url: url,
       page: page,
@@ -30,7 +38,28 @@ defmodule Systems.DataDonation.CenterdataController do
       token: token
     }
 
-    path = Routes.live_path(conn, Systems.DataDonation.UploadPage, id, session: session)
+    Keyword.put(opts, :session, session)
+  end
+
+  defp add_locale(opts, %{"locale" => locale}), do: add_locale(opts, locale)
+  defp add_locale(opts, %{"lang" => locale}), do: add_locale(opts, locale)
+
+  defp add_locale(opts, locale) when is_binary(locale) do
+    if is_supported?(locale) do
+      Keyword.put(opts, :locale, locale)
+    else
+      opts
+    end
+  end
+
+  defp add_locale(opts, _), do: opts
+
+  defp is_supported?(locale) when is_binary(locale) do
+    locale in @supported_locales
+  end
+
+  defp start_data_donation(opts, conn, id) do
+    path = Routes.live_path(conn, Systems.DataDonation.UploadPage, id, opts)
     redirect(conn, to: path)
   end
 end
