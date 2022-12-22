68ee2974a0bcf5e3890e16eeb6e16ff305eeb420
emielvdveen <emiel@applike.nl>
2022-12-22 16:57:28 +0100

    Disable CRSF check in POST from Centerdata

--- core/systems/data_donation/routes.ex
+++ core/systems/data_donation/routes.ex
@@ -23,9 +23,13 @@ defmodule Systems.DataDonation.Routes do
         live("/thanks/:id/:participant", ThanksPage)
         live("/thankswhatsapp/:id/:participant", ThanksWhatsappPage)
 
-        post("/centerdata/:id", CenterdataController, :create)
         get("/centerdata/:id", CenterdataController, :create)
         live("/centerdata/fakeapi/page", CenterdataFakeApiPage)
+      end
+
+      scope "/data-donation", Systems.DataDonation do
+        pipe_through([:browser_unprotected])
+        post("/centerdata/:id", CenterdataController, :create)
       end
     end
   end
