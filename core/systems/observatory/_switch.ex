284d28fe936eb854414cdfb09a82f7d50d7c2d92
emielvdveen <emiel@applike.nl>
2023-11-16 17:15:13 +0100

    Change local_broadcast to broadcast (all nodes in cluster)

--- core/systems/observatory/_switch.ex
+++ core/systems/observatory/_switch.ex
@@ -6,6 +6,6 @@ defmodule Systems.Observatory.Switch do
   }
 
   def intercept({:page, page}, %{id: id} = message) do
-    Observatory.Public.local_dispatch(page, [id], message)
+    Observatory.Public.dispatch(page, [id], message)
   end
 end
