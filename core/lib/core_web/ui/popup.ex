9983f7e01c3750fa78eceea4853a8b7f7773cac3
emielvdveen <emiel@applike.nl>
2023-12-08 13:47:15 +0100

    Fixed unwanted restart of Feldspar after showing/hiding popup

--- core/lib/core_web/ui/popup.ex
+++ core/lib/core_web/ui/popup.ex
@@ -1,13 +1,29 @@
 defmodule CoreWeb.UI.Popup do
   use CoreWeb, :html
 
-  slot(:inner_block, required: true)
+  slot(:inner_block, default: nil)
 
   def popup(assigns) do
     ~H"""
-    <div class="fixed z-20 left-0 top-0 w-full h-full bg-black bg-opacity-30">
+    <div class={"fixed z-20 left-0 top-0 w-full h-full bg-black bg-opacity-30 #{if @inner_block do "block" else "hidden" end}"}>
       <div class="flex flex-row items-center justify-center w-full h-full">
         <%= render_slot(@inner_block) %>
+      </div>
+    </div>
+    """
+  end
+
+  attr(:popup, :map, default: nil)
+
+  def takeover(assigns) do
+    ~H"""
+    <div class={"fixed z-20 left-0 top-0 w-full h-full bg-black bg-opacity-30 #{if @popup do "block" else "hidden" end}"}>
+      <div class="flex flex-row items-center justify-center w-full h-full">
+        <%= if @popup do %>
+          <div class="w-3/5 h-4/5 overflow-y-scroll">
+            <.live_component id={:page_popup} module={@popup.module} {@popup.params} />
+          </div>
+        <% end %>
       </div>
     </div>
     """
