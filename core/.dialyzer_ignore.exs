1efe932ede21f8bb2e47de7c6234b692cbe67cfb
Emiel <emielvdveen@gmail.com>
2024-09-11 18:10:05 +0200

    Fixed dialyzer warnings in test env V

--- core/.dialyzer_ignore.exs
+++ core/.dialyzer_ignore.exs
@@ -1,7 +1,5 @@
 [
   # https://github.com/phoenixframework/phoenix/issues/5437, fixed in Phoenix 1.7.3 or higher
   {"systems/benchmark/export_controller.ex", :no_return},
-  {"systems/benchmark/export_controller.ex", :call},
-  # https://elixirforum.com/t/dialyzer-listed-not-implemented-protocols-as-unknown-functions/2099/12
-  ~r/.*unknown_function.*/
+  {"systems/benchmark/export_controller.ex", :call}
 ]
