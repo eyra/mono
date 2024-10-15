94e2cd520d29c88593c32707909524d89bbd6741
Emiel <emielvdveen@gmail.com>
2024-10-15 11:39:33 +0200

    #976 Added snapshat support

--- core/systems/workflow/platforms.ex
+++ core/systems/workflow/platforms.ex
@@ -1,6 +1,10 @@
 defmodule Systems.Workflow.Platforms do
   @moduledoc """
   Defines DDP platforms that are supported out of the box.
+  Make sure to add icons for each platform in the `priv/static/images/icons` directory:
+  * <platform>.svg
+  * <platform>_square.svg
+  See Figma https://www.figma.com/design/RXKuvMFGz3Eln5MNPJ1q6a/Design-system?node-id=470-10741&t=XxCVZd4kNTsfcQP7-1 for reference.
   """
   use Core.Enums.Base,
       {:platforms,
@@ -11,6 +15,7 @@ defmodule Systems.Workflow.Platforms do
          :instagram,
          :netflix,
          :samsung,
+         :snapchat,
          :tiktok,
          :whatsapp,
          :x,
