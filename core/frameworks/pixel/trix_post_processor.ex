5dc0b3bb02d99ac17aca87e2cc8e207042369f03
MelchiorKokernoot <melchiorkokernoot@gmail.com>
2024-10-17 14:04:09 +0200

    feat: add docs and test for trix post processor

--- core/frameworks/pixel/trix_post_processor.ex
+++ core/frameworks/pixel/trix_post_processor.ex
@@ -7,6 +7,12 @@ defmodule Frameworks.Pixel.TrixPostProcessor do
   @doc """
   Adds `target="_blank"` to any `<a>` tags that do not already have it.
   It returns the modified HTML content.
+
+  ## Regex Breakdown:
+  - `<a`: Matches the opening part of an <a> tag.
+  - `(?![^>]*target=)`: Negative lookahead to ensure the <a> tag does not already have a `target` attribute.
+  - `[^>]*`: Ensures that no characters up to the closing `>` of the tag contain `target=`.
+  - `([^>]*)`: Captures the remaining attributes inside the <a> tag.
   """
   def add_target_blank(html_content) do
     Regex.replace(~r/<a(?![^>]*target=)([^>]*)>/, html_content, "<a\\1 target=\"_blank\">")
