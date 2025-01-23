37152039e87188cb41c563f2b8d94949e3393d47
MelchiorKokernoot <melchiorkokernoot@gmail.com>
2025-01-23 12:09:10 +0100

    chore: remove unused variable

--- core/systems/project/_private.ex
+++ core/systems/project/_private.ex
@@ -2,6 +2,6 @@ defmodule Systems.Project.Private do
   alias Systems.Project
 
   def get_project_key(%Project.Model{root: %Project.NodeModel{id: id}} = _project) do
-    key = "project_node=#{id}"
+    "project_node=#{id}"
   end
 end
