f2dbb5a41430c031887a81b49640368ba0c6a17a
Emiel <emielvdveen@gmail.com>
2025-02-26 19:57:13 +0100

    Fixed Project.NodePage tabbar_id

--- core/systems/project/node_page.ex
+++ core/systems/project/node_page.ex
@@ -14,9 +14,10 @@ defmodule Systems.Project.NodePage do
   end
 
   @impl true
-  def mount(params, _session, socket) do
+  def mount(%{"id" => id} = params, _session, socket) do
     initial_tab = Map.get(params, "tab")
-    {:ok, socket |> assign(initial_tab: initial_tab)}
+    tabbar_id = "node_page/#{id}"
+    {:ok, socket |> assign(initial_tab: initial_tab, tabbar_id: tabbar_id)}
   end
 
   def handle_event(:should_flash_message, %{status: status, message: message}, socket) do
@@ -33,8 +34,8 @@ defmodule Systems.Project.NodePage do
     <.tabbar_page
         title={@vm.title}
         tabs={@vm.tabs}
-        tabbar_id={@vm.tabbar_id}
         show_errors={@vm.show_errors}
+        tabbar_id={@tabbar_id}
         initial_tab={@initial_tab}
         menus={@menus}
         modals={@modals}
