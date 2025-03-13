6c520f4a36e4055d520453af9c161c8baf6517e3
Tjerk Nan <t.nan@eyra.co>
2025-03-13 16:06:34 +0100

    Add support for revision parameter

--- core/config/runtime.exs
+++ core/config/runtime.exs
@@ -146,6 +146,7 @@ if config_env() == :prod do
       otp_app: :core,
       name: "Next",
       env: app_domain,
+      release: System.get_env("APPSIGNAL_REVISION"),
       push_api_key: push_api_key,
       active: true
   end
