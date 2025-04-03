28ae0250b5ea44cde90adc6bd641b90c0da0d776
Melchior Kokernoot <melchiorkokernoot@gmail.com>
2025-04-03 09:00:17 +0000

    chore: update styles for transition state between mobile and desktop

--- core/systems/home/view.ex
+++ core/systems/home/view.ex
@@ -18,7 +18,6 @@ defmodule Systems.Home.View do
   def render(assigns) do
     ~H"""
     <div>
-
       <div class="bg-grey6">
         <%= render_intro(assigns) %>
         <%= render_steps(assigns) %>
@@ -31,27 +30,27 @@ defmodule Systems.Home.View do
 
   defp render_intro(assigns) do
     ~H"""
-    <section class="py-6 sm:py-12 bg-white">
-      <div class="flex flex-col gap-8 md:flex-row justify-between sm:mt-12 md:pl-14">
-        <div class="w-full px-6 sm:px-0 sm:text-center md:text-left md:pr-12 xl:pr-0">
-          <h1 class="text-title3 sm:text-title1 font-title1 font-bold text-grey1">
+    <section class="py-6 lg:py-12 bg-white">
+      <div class="flex gap-8 lg:flex-row justify-between lg:mt-12 lg:pl-14">
+        <div class="w-full px-6 lg:px-0 text-left lg:pr-12 xl:pr-0">
+          <h1 class="text-title3 lg:text-title1 font-title1 font-bold text-grey1">
             What's Next?
           </h1>
-          <p class="mt-6 text-xl sm:text-2xl font-intro leading-7">
-          The Next platform is an open and inclusive platform designed to accelerate research and knowledge creation.
-          By integrating cutting-edge software services, Next empowers researchers with sustainable and privacy-conscious tools.
-          Built on open-source principles and co-created with researchers, Next fosters innovation, transparency, and collaboration.
+          <p class="mt-6 text-xl lg:text-2xl font-intro leading-7">
+            The Next platform is an open and inclusive platform designed to accelerate research and knowledge creation.
+            By integrating cutting-edge software services, Next empowers researchers with sustainable and privacy-conscious tools.
+            Built on open-source principles and co-created with researchers, Next fosters innovation, transparency, and collaboration.
           </p>
           <div class="flex justify-start mt-8">
             <Button.dynamic
-              action={%{type: :redirect, to: "https://www.eyra.co/next-software-services"}}
+              action={%{type: :redirect, to: "/next-software-services"}}
               face={%{type: :primary, label: "Learn more"}}
             />
             </div>
           </div>
-        </div>
-        <div class="w-full hidden hidden md:block pr-12">
-            <img src={~p"/images/landing_page/drawing.svg"} alt="Image displaying " class="w-full max-h-96" />
+          <div class="w-full hidden hidden lg:block pr-12">
+              <img src={~p"/images/landing_page/drawing.svg"} alt="Image displaying " class="w-full max-h-96" />
+          </div>
         </div>
     </section>
     """
@@ -59,11 +58,11 @@ defmodule Systems.Home.View do
 
   defp render_steps(assigns) do
     ~H"""
-    <section class="py-6 sm:py-16 sm:px-6">
+    <section class="py-6 lg:py-16 lg:px-6">
         <h2 class="text-title3 font-title3 pl-6 text-grey-1">
           How to get started?
         </h2>
-        <div class="mt-6 justify-around w-full flex flex-col md:flex-row gap-8 lg:gap-16 px-6">
+        <div class="mt-6 justify-around w-full flex flex-col lg:flex-row gap-8 lg:gap-16 px-6 ">
           <%= render_step_card(%{
                 image: ~p"/images/landing_page/geel-abstract.svg",
                 title: "Sign up",
@@ -92,7 +91,7 @@ defmodule Systems.Home.View do
     ~H"""
     <div class="bg-white w-full rounded overflow-hidden drop-shadow-lg">
       <img
-        class="w-full h-32 sm:h-46 lg:min-h-80 2xl:min-h-80 object-cover"
+        class="w-full h-32 lg:h-48 object-cover"
         src={@image}
         alt={@title}
       />
@@ -105,7 +104,7 @@ defmodule Systems.Home.View do
             <%= @title %>
           </h3>
         </div>
-        <p class="mt-4 text-lg sm:text-introdesktop ">
+        <p class="mt-4 text-lg lg:text-introdesktop ">
           <%= @description %>
         </p>
         <%= if assigns[:button] do %>
@@ -123,21 +122,21 @@ defmodule Systems.Home.View do
 
   defp render_available_services(assigns) do
     ~H"""
-    <section class="h-full pt-6 sm:pt-12 px-6 bg-white">
-      <div class="sm:px-12 flex-col gap-10 sm:flex-row justify-start items-center md:gap-10">
-          <img class="hidden sm:block h-64" src={~p"/images/landing_page/services-drawing.png"}/>
-          <div class="flex flex-col justify-start items-start sm:pr-8 sm:pr-20">
-              <div class="pb-6 sm:pb-8 text-title4 font-title4 sm:text-title1 sm:font-title1 text-grey1">
+    <section class="h-full pt-6 lg:pt-12 px-6 bg-white">
+      <div class="lg:px-12 flex lg:flex-row justify-start items-center lg:gap-10">
+          <img class="h-64 hidden lg:block" src={~p"/images/landing_page/services-drawing.png"}/>
+          <div class="flex flex-col justify-start items-start lg:pr-8 lg:pr-20">
+              <div class="pb-6 lg:pb-8 text-title4 font-title4 lg:text-title1 lg:font-title1 text-grey1">
                 Available services
               </div>
               <div class="pb-6 flex flex-col justify-start items-start">
-                  <div class="pb-6 justify-center text-xl sm:text-2xl font-intro leading-7">
+                  <div class="pb-6 justify-center text-xl lg:text-2xl font-intro leading-7">
                     The Next platform is an open and inclusive platform designed to accelerate
                     research and knowledge creation. By integrating cutting-edge software
                     services, Next empowers researchers with sustainable and
                     privacy-conscious tools.
                   </div>
-                  <div class="sm:pt-16">
+                  <div class="lg:pt-16">
                     <Button.dynamic
                       action={%{type: :redirect, to: "/next-software-services"}}
                       face={%{type: :primary, label: "Available services"}}
@@ -152,13 +151,13 @@ defmodule Systems.Home.View do
 
   defp render_video(assigns) do
     ~H"""
-    <section class="h-full pt-8 sm:pt-24 px-6 bg-white">
-        <h1 class="text-title3 font-title3 sm:text-title1 sm:font-title1 text-center text-grey1">
-          Wil je weten hoe het werkt?
+    <section class="h-full pt-8 lg:pt-24 px-6 bg-white">
+        <h1 class="text-title3 font-title3 lg:text-title1 lg:font-title1 text-center text-grey1">
+          Want to know how this works?
         </h1>
         <div class="flex justify-center pt-6">
           <iframe
-            class="h-1/2 sm:h-[60vh] w-full rounded-lg mt-6 shadow"
+            class="h-1/2 lg:h-[60vh] w-full rounded-lg mt-6 shadow"
             src="https://www.youtube.com/embed/dQw4w9WgXcQ"
             title="Sneak preview"
             allowfullscreen
