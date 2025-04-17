28b1876d6d8925deb9abfa31f8d3ed54ce674b2f
MelchiorKokernoot <melchiorkokernoot@gmail.com>
2025-04-17 15:10:34 +0200

    chore: add gettext translations for home page

--- core/systems/home/view.ex
+++ core/systems/home/view.ex
@@ -34,22 +34,20 @@ defmodule Systems.Home.View do
       <div class="flex gap-8 lg:flex-row justify-between lg:mt-12 lg:pl-14">
         <div class="w-full px-6 lg:px-0 text-left lg:pr-12 xl:pr-0">
           <h1 class="text-title3 lg:text-title1 font-title1 font-bold text-grey1">
-            What's Next?
+            <%= dgettext("eyra-crew", "home.intro.title") %>
           </h1>
           <p class="mt-6 text-xl lg:text-2xl font-intro leading-7">
-            The Next platform is an open and inclusive platform designed to accelerate research and knowledge creation.
-            By integrating cutting-edge software services, Next empowers researchers with sustainable and privacy-conscious tools.
-            Built on open-source principles and co-created with researchers, Next fosters innovation, transparency, and collaboration.
+            <%= dgettext("eyra-crew", "home.intro.description") %>
           </p>
           <div class="flex justify-start mt-8">
             <Button.dynamic
               action={%{type: :redirect, to: "/next-software-services"}}
-              face={%{type: :primary, label: "Learn more"}}
+              face={%{type: :primary, label: dgettext("eyra-crew", "home.intro.learn_more_button")}}
             />
             </div>
           </div>
           <div class="w-full hidden hidden lg:block pr-12">
-              <img src={~p"/images/landing_page/drawing.svg"} alt="Image displaying " class="w-full max-h-96" />
+              <img src={~p"/images/landing_page/drawing.svg"} alt={dgettext("eyra-crew", "home.intro.image_alt")} class="w-full max-h-96" />
           </div>
         </div>
     </section>
@@ -60,27 +58,27 @@ defmodule Systems.Home.View do
     ~H"""
     <section class="py-6 lg:py-16 lg:px-6">
         <h2 class="text-title3 font-title3 pl-6 text-grey-1">
-          How to get started?
+          <%= dgettext("eyra-crew", "home.steps.title") %>
         </h2>
         <div class="mt-6 justify-around w-full flex flex-col lg:flex-row gap-8 lg:gap-16 px-6 ">
           <%= render_step_card(%{
                 image: ~p"/images/landing_page/geel-abstract.svg",
-                title: "Sign up",
+                title: dgettext("eyra-crew", "home.steps.step1.title"),
                 stepNumber: 1,
-                description: "Create your free account and gain access to the Next platform with readily available software services.",
-                button: %{label: "Create account", link: "/user/signup/creator"}
+                description: dgettext("eyra-crew", "home.steps.step1.description"),
+                button: %{label: dgettext("eyra-crew", "home.steps.step1.button"), link: "/user/signup/creator"}
                }) %>
           <%= render_step_card(%{
                 image: ~p"/images/landing_page/hout-abstract.svg",
-                title: "Configure",
+                title: dgettext("eyra-crew", "home.steps.step2.title"),
                 stepNumber: 2,
-                description: "Choose one of the software services and configure it to fit your research needs."
+                description: dgettext("eyra-crew", "home.steps.step2.description")
                }) %>
           <%= render_step_card(%{
                 image: ~p"/images/landing_page/blauw-abstract.svg",
-                title: "Preview",
+                title: dgettext("eyra-crew", "home.steps.step3.title"),
                 stepNumber: 3,
-                description: "See your configuration in action, make adjustments, and immediately observe the effect."
+                description: dgettext("eyra-crew", "home.steps.step3.description")
                }) %>
         </div>
     </section>
@@ -124,22 +122,19 @@ defmodule Systems.Home.View do
     ~H"""
     <section class="h-full pt-6 lg:pt-12 px-6 bg-white">
       <div class="lg:px-12 flex lg:flex-row justify-start items-center lg:gap-10">
-          <img class="h-64 hidden lg:block" src={~p"/images/landing_page/services-drawing.png"}/>
+          <img class="h-64 hidden lg:block" src={~p"/images/landing_page/services-drawing.png"} alt={dgettext("eyra-crew", "home.services.image_alt")}/>
           <div class="flex flex-col justify-start items-start lg:pr-8 lg:pr-20">
               <div class="pb-6 lg:pb-8 text-title4 font-title4 lg:text-title1 lg:font-title1 text-grey1">
-                Available services
+                <%= dgettext("eyra-crew", "home.services.title") %>
               </div>
               <div class="pb-6 flex flex-col justify-start items-start">
                   <div class="pb-6 justify-center text-xl lg:text-2xl font-intro leading-7">
-                    The Next platform is an open and inclusive platform designed to accelerate
-                    research and knowledge creation. By integrating cutting-edge software
-                    services, Next empowers researchers with sustainable and
-                    privacy-conscious tools.
+                    <%= dgettext("eyra-crew", "home.services.description") %>
                   </div>
                   <div class="lg:pt-16">
                     <Button.dynamic
                       action={%{type: :redirect, to: "/next-software-services"}}
-                      face={%{type: :primary, label: "Available services"}}
+                      face={%{type: :primary, label: dgettext("eyra-crew", "home.services.button")}}
                     />
                   </div>
               </div>
@@ -153,13 +148,13 @@ defmodule Systems.Home.View do
     ~H"""
     <section class="h-full pt-8 lg:pt-24 px-6 bg-white">
         <h1 class="text-title3 font-title3 lg:text-title1 lg:font-title1 text-center text-grey1">
-          Want to know how this works?
+          <%= dgettext("eyra-crew", "home.video.title") %>
         </h1>
         <div class="flex justify-center pt-6">
           <iframe
             class="h-1/2 lg:h-[60vh] w-full rounded-lg mt-6 shadow"
             src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-            title="Sneak preview"
+            title={dgettext("eyra-crew", "home.video.preview_title")}
             allowfullscreen
           >
           </iframe>
