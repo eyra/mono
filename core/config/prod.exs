dddf0deff747639a11eb2750377846a638c25ca4
MelchiorKokernoot <melchiorkokernoot@gmail.com>
2025-05-29 10:17:26 +0200

    chore: remove x-proto rewrite

--- core/config/prod.exs
+++ core/config/prod.exs
@@ -12,4 +12,4 @@ config :core, SurfConext,
   client_id: System.get_env("SURFCONEXT_CLIENT_ID")
 
 config :core, CoreWeb.Endpoint,
-  force_ssl: [rewrite_on: [:x_forwarded_host, :x_forwarded_port, :x_forwarded_proto]]
+  force_ssl: [rewrite_on: [:x_forwarded_host, :x_forwarded_port]]
