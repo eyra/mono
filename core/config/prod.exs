ffcc921d554b33d99e984487df6e8667274a47b4
MelchiorKokernoot <MelchiorKokernoot@users.noreply.github.com>
2025-05-29 08:18:32 +0000

    Formatted code

--- core/config/prod.exs
+++ core/config/prod.exs
@@ -11,5 +11,4 @@ config :core, SurfConext,
   site: "https://connect.test.surfconext.nl",
   client_id: System.get_env("SURFCONEXT_CLIENT_ID")
 
-config :core, CoreWeb.Endpoint,
-  force_ssl: [rewrite_on: [:x_forwarded_host, :x_forwarded_port]]
+config :core, CoreWeb.Endpoint, force_ssl: [rewrite_on: [:x_forwarded_host, :x_forwarded_port]]
