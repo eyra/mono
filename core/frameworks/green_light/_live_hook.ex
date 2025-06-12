e33c171d358cc3f7b09e8fd8a98d1442e4ef9156
Emiel <emielvdveen@gmail.com>
2025-06-12 20:29:49 +0200

    Added some debug logging

--- core/frameworks/green_light/_live_hook.ex
+++ core/frameworks/green_light/_live_hook.ex
@@ -12,6 +12,7 @@ defmodule Frameworks.GreenLight.LiveHook do
     if access_allowed?(live_view_module, params, session, socket) do
       {:cont, socket}
     else
+      Logger.info("Access denied: access not allowed")
       {:halt, redirect(socket, to: ~p"/access_denied")}
     end
   end
@@ -30,6 +31,10 @@ defmodule Frameworks.GreenLight.LiveHook do
       user && Logger.info("User #{user.id} can_access? #{live_view_module}: #{can_access?}")
       can_access?
     else
+      Logger.info(
+        "Access denied: function_exported?(:get_authorization_context, 3) is false, module=#{inspect(live_view_module)}"
+      )
+
       auth_module().can_access?(user, live_view_module)
     end
   end
