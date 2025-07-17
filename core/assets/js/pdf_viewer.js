fa838a2fc5ebfcae66f68925f959c3be024bb86d
MelchiorKokernoot <melchiorkokernoot@gmail.com>
2025-07-17 16:09:17 +0200

    chore: bump pdfjs version, start using local worker instead of cdn worker

--- core/assets/js/pdf_viewer.js
+++ core/assets/js/pdf_viewer.js
@@ -1,8 +1,7 @@
 import _ from "lodash";
 
-const pdfjsVersion = "3.11.174";
 const pdfjs = require("../node_modules/pdfjs-dist");
-const worker = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsVersion}/pdf.worker.min.js`;
+const worker = "/assets/pdf.worker.min.mjs";
 
 function renderPages(hook) {
   hook.renderPages();
