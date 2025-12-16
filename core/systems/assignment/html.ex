53125a1e3db8141481ed813bf628da39342cd7f0
Melle Lieuwes <m.lieuwes@eyra.co>
2025-12-16 20:20:55 +0100

    Participant task list shows vertical bar in all browsers on MacOS

--- core/systems/assignment/html.ex
+++ core/systems/assignment/html.ex
@@ -8,7 +8,7 @@ defmodule Systems.Assignment.Html do
       <div class="w-full h-full md:flex md:flex-row md:justify-center md:pt-12 lg:pt-20">
         <div class="w-full md:max-w-[640px] px-6 pt-6 md:px-0 flex-shrink-0 flex flex-col gap-6">
           <div class="flex-grow">
-            <div class="h-full overflow-y-scroll">
+            <div class="h-full overflow-y-auto">
               <%= render_slot(@inner_block) %>
             </div>
           </div>
