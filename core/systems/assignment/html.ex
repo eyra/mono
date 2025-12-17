7beeedf1aa76c7997ecc9b074cb5d7d146d9a10c
Melle Lieuwes <m.lieuwes@eyra.co>
2025-12-17 11:04:47 +0100

    Removed redundant scrollbar from participant tasklist

--- core/systems/assignment/html.ex
+++ core/systems/assignment/html.ex
@@ -6,12 +6,8 @@ defmodule Systems.Assignment.Html do
   def task_list(assigns) do
     ~H"""
       <div class="w-full h-full md:flex md:flex-row md:justify-center md:pt-12 lg:pt-20">
-        <div class="w-full md:max-w-[640px] px-6 pt-6 md:px-0 flex-shrink-0 flex flex-col gap-6">
-          <div class="flex-grow">
-            <div class="h-full overflow-y-auto">
-              <%= render_slot(@inner_block) %>
-            </div>
-          </div>
+        <div class="w-full md:max-w-[640px] px-6 pt-6 md:px-0 flex-shrink-0">
+          <%= render_slot(@inner_block) %>
         </div>
       </div>
     """
