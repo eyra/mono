bcec55727ed89aa20bdedb33b36ace8d8938cfa0
Jeroen <jeroen@adaptiveconstant.com>
2026-01-23 22:13:27 +0100

    Ignore Wallaby functions in Dialyzer
    Wallaby is a test-only dependency used in feature_case.ex.
    Dialyzer can't find its functions at compile time, causing
    false positive warnings.

--- core/.dialyzer_ignore.exs
+++ core/.dialyzer_ignore.exs
@@ -3,5 +3,7 @@
   {"systems/benchmark/export_controller.ex", :no_return},
   {"systems/benchmark/export_controller.ex", :call},
   # https://elixirforum.com/t/dialyzer-listed-not-implemented-protocols-as-unknown-functions/2099/12
-  ~r/.*:unknown_function Function .*__impl__\/1 does not exist.*/
+  ~r/.*:unknown_function Function .*__impl__\/1 does not exist.*/,
+  # Wallaby is a test-only dependency, Dialyzer can't find its functions
+  ~r/test\/support\/feature_case.ex.*unknown_function Function Wallaby\..*/
 ]
